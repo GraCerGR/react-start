3510f0c03d13dcf7711507dc50580ccda6509f60
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-05-29 19:24:23 +0700

    Теперь массив папок сортируется и отображается в соответсвии с иерархией. Не понимаю, почему часть файлов их /node_modules не игнорируется git

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -6,16 +6,18 @@ const TreeComponent = () => {
 //-------------------------Создание папок с правильным путём--------------------------------
 
   const addFolder = (parentId) => {
-    console.log(parentId);
     const childrenOfParent = tree.filter(folder => folder.id.startsWith(parentId + '.') && folder.id !== parentId && folder.id.split('.').length === parentId.split('.').length + 1);
     const lastChildIndex = childrenOfParent.length > 0 ? Math.max(...childrenOfParent.map(folder => parseInt(folder.id.split('.').pop(), 10))) : 0;
-    console.log(childrenOfParent);
-    console.log(lastChildIndex);
     const newFolder = {
       id: parentId === 'root' ? `${parentId}.${lastChildIndex + 1}` : `${parentId}.${lastChildIndex + 1}`,
       name: parentId === 'root' ? `${parentId}.${lastChildIndex + 1}` : `${parentId}.${lastChildIndex + 1}`
     };
-    setTree([...tree, newFolder]);
+    const updatedTree = [...tree, newFolder].sort((a, b) => {
+        return a.id.localeCompare(b.id);
+      });
+  
+      setTree(updatedTree);
+    console.log(tree);
   };
 
 //---------------------------------------------------------
